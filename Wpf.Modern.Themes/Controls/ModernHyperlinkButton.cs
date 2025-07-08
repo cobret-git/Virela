c9b1be8651a3c8afa8bf5d994cf9e233a80e8262
Kostia Britikov <konstantin.britikov@gmail.com>
2025-07-08 16:29:12 +0200

    Added ModernHyperlinkButton control for URI navigation
    This commit introduces a new `ModernHyperlinkButton` control to the WPF application, which inherits from `ModernButton` and allows navigation to specified URIs upon clicking. Key changes include:
    
    - Added `ModernHyperlinkButton` section in **ButtonPage.xaml** with enabled and disabled states.
    - Defined a new style for `ModernHyperlinkButton` in **Generic.xaml** and various theme files to ensure consistent theming.
    - Created `GithubHyperlinkButtonStyle` in **ButtonStyles.xaml**, specifying properties for padding, background brushes, and interaction triggers.
    - Implemented the `ModernHyperlinkButton` class in **ModernHyperlinkButton.cs**, including a dependency property for `NavigateUri` and a click event handler to open links in the default web browser.
    
    closes #6

---
+++ Wpf.Modern.Themes/Controls/ModernHyperlinkButton.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace Wpf.Modern.Themes.Controls
+{
+    public class ModernHyperlinkButton : ModernButton
+    {
+        #region Constructors
+        static ModernHyperlinkButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ModernHyperlinkButton), new FrameworkPropertyMetadata(typeof(ModernHyperlinkButton)));
+        }
+        public ModernHyperlinkButton()
+        {
+            Click += OnClick;
+        }
+        #endregion
+
+        #region Dependency Properties
+        public static readonly DependencyProperty NavigateUriProperty =
+            DependencyProperty.Register(
+                "NavigateUri",
+                typeof(Uri),
+                typeof(ModernHyperlinkButton),
+                new PropertyMetadata(null));
+        #endregion
+
+        #region Properties
+        public Uri NavigateUri
+        {
+            get { return (Uri)GetValue(NavigateUriProperty); }
+            set { SetValue(NavigateUriProperty, value); }
+        }
+        #endregion
+
+        #region Handlers
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            if (NavigateUri != null)
+            {
+                try
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = NavigateUri.ToString(),
+                        UseShellExecute = true
+                    });
+                }
+                catch (Exception ex)
+                {
+                    // Handle exception as needed (log, show message, etc.)
+                    Debug.WriteLine($"Unable to open link: {ex.Message}", "Error");
+                }
+            }
+        }
+        #endregion
+    }
+}
